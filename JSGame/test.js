0e3fd0a990508cbc2404449d48ad49349f13f1f9
Taketio <take.ch3@gmail.com>
2017-05-31 08:30:53 +0900

    test

--- JSGame/test.js
+++ JSGame/test.js
@@ -9,6 +9,8 @@ window.onload = function(){
 
     //画像の読み込み
     game.preload("img/puyos.png");
+    game.preload("img/box.png");
+    game.preload("img/wood_test.png");
     //音声の読み込み
     game.preload("sounds/bgm07.wav");
 
@@ -21,18 +23,18 @@ window.onload = function(){
         game.rootScene.addChild(hello);
 
         //文字列の描画
-            var hello = new Label("Hello Bear");
-            hello.x = 10;
-            hello.y = 150;
-            game.rootScene.addChild(hello);
+        var hello = new Label("Hello Bear");
+        hello.x = 10;
+        hello.y = 150;
+        game.rootScene.addChild(hello);
 
         //画像の描画
-            var sprite = new Sprite(16, 16);
-            sprite.image = game.assets["img/puyos.png"];
-            game.rootScene.addChild(sprite);
+        var sprite = new Sprite(16, 16);
+        sprite.image = game.assets["img/puyos.png"];
+        game.rootScene.addChild(sprite);
 
         //音声の再生
-            game.assets["sounds/bgm07.wav"].play();
+        game.assets["sounds/bgm07.wav"].play();
 
         //フレームの確認
         game.addEventListener("enterframe",function(){
@@ -45,21 +47,84 @@ window.onload = function(){
         });
 
          //画面をタップした時の距離
-        sprite.addEventListener("touchstart",function(){
+         sprite.addEventListener("touchstart",function(){
             game.replaceScene(main);
         });
 
 
         //2つ目のシーン(ゲームのメインになるシーン)
         var main = new Scene();
-        main.backgroundColor = "#FF9999";
+        main.backgroundColor = "#FF0000";
         //シーンにメッセージを登録
         var secondMessage = new Label("Hello, secondScene");
         secondMessage.x = 10;
         secondMessage.y = 10;
         main.addChild(secondMessage);
 
-       
+        //画像の描画
+        var tresureBox = [];
+        var wood_bar = [];
+
+        for(var i=0; i< 2 ; ++i){
+
+            wood_bar[i] = new Sprite(480,20);
+            wood_bar[i].image = game.assets["img/wood_test.png"];
+            wood_bar[i].x = 0;
+            wood_bar[i].y = 120*i + 150;
+            main.addChild(wood_bar[i]);
+        }
+
+
+        for(var i=0; i< 6; ++i){
+            var ind = i;
+            console.log("test : " + ind);
+                //画像の大きさを取得
+                tresureBox[i] = new Sprite(100,70);
+                //画像設定
+                tresureBox[i].image = game.assets["img/box.png"];
+                    //位置を指定
+                    tresureBox[i].x = 150*(i%3) + 40; 
+
+                    tresureBox[i].y = 120*(i%2) + 80;
+
+
+                    main.addChild(tresureBox[i]);
+
+                }
+
+
+
+        //宝箱のクリック設定ーーなぜか直書きじゃないと反映されない
+        tresureBox[0].addEventListener("touchstart",function(){
+            tresureBox[0].tl.moveTo(120,120,30);
+        });
+        tresureBox[1].addEventListener("touchstart",function(){
+            tresureBox[1].tl.moveTo(120,120,30);
+        });
+        tresureBox[2].addEventListener("touchstart",function(){
+            tresureBox[2].tl.moveTo(120,120,30);
+        });
+        tresureBox[3].addEventListener("touchstart",function(){
+            tresureBox[3].tl.moveTo(120,120,30);
+        });
+        tresureBox[4].addEventListener("touchstart",function(){
+            tresureBox[4].tl.moveTo(120,120,30);
+        });
+        tresureBox[5].addEventListener("touchstart",function(){
+            tresureBox[5].tl.moveTo(120,120,30);
+        });
+
+
+
+
+            　 //  main.addEventListener("touchstart",function(){
+              //  game.pushScene(result);
+           // });
+
+
+
+
+
 
         //3つ目のシーン（結果発表画面）
         var result = new Scene();
@@ -70,9 +135,6 @@ window.onload = function(){
         thirdMessage.y = 100;
         result.addChild(thirdMessage);
 
-         main.addEventListener("touchstart",function(){
-            game.pushScene(result);
-        });
 
 
         result.addEventListener("touchstart",function(){
@@ -80,7 +142,7 @@ window.onload = function(){
         });
 
 
-       
+
 
 
 
